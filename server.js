8188281b1cf1058c3413eb2bc74df98a131b6177
bethea28 <your-email@example.com>
2025-12-08 18:02:49 -0500

    Add sequelize-cli and update server message

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
 });
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World! bryan");
 });
 app.use("/auth", authRoutes);
 app.use("/stories", storyRoutes);
